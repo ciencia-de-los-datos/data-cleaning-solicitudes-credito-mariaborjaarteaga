b56454c81f513899ef16b166ec2cc4daa85da390
María José Borja Arteaga <101903233+mariaborjaarteaga@users.noreply.github.com>
2023-10-24 15:44:32 -0500

    Letras en minúsculas

--- pregunta.py
+++ pregunta.py
@@ -16,5 +16,11 @@ def clean_data():
     #
     # Inserte su código aquí
     #
+    #parar todas las letras a low_case
+    df['sexo'] = df['sexo'].str.lower()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['barrio'] = df['barrio'].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.lower()
 
     return df
