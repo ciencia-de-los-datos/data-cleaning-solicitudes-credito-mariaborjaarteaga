557f87cfc4f4a35c750abf12cec16947121da2e5
María José Borja Arteaga <101903233+mariaborjaarteaga@users.noreply.github.com>
2023-10-24 15:46:04 -0500

    Raya al pisp

--- pregunta.py
+++ pregunta.py
@@ -16,11 +16,15 @@ def clean_data():
     #
     # Inserte su código aquí
     #
-    #parar todas las letras a low_case
+    #Eeemplazar todas las letras a low_case
     df['sexo'] = df['sexo'].str.lower()
     df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
     df['idea_negocio'] = df['idea_negocio'].str.lower()
     df['barrio'] = df['barrio'].str.lower()
     df['línea_credito'] = df['línea_credito'].str.lower()
+
+    ## Reemplazar todas las raya al piso por espacio
+
+    df['nombre_columna'] = df['nombre_columna'].str.replace('_', ' ')
 
     return df
