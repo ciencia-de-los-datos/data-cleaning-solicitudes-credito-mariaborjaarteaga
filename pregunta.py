c67328d28f5bb56bfad98ebe6edced85c15a7987
María José Borja Arteaga <101903233+mariaborjaarteaga@users.noreply.github.com>
2023-10-25 11:23:21 -0500

    Cargar respuesta
    Cargar respuesta

--- pregunta.py
+++ pregunta.py
@@ -17,14 +17,28 @@ def clean_data():
     # Inserte su código aquí
     #
     #Eeemplazar todas las letras a low_case
-    df['sexo'] = df['sexo'].str.lower()
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
-    df['idea_negocio'] = df['idea_negocio'].str.lower()
-    df['barrio'] = df['barrio'].str.lower()
-    df['línea_credito'] = df['línea_credito'].str.lower()
-
-    ## Reemplazar todas las raya al piso por espacio
-
-    df['nombre_columna'] = df['nombre_columna'].str.replace('_', ' ')
-
+    df = pd.read_csv(archivo_csv, delimiter=';',index_col=0)
+    df.dropna(inplace=True)
+    
+    #df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
+    def unificar_fecha(fecha):
+        try:
+            return pd.to_datetime(fecha,  dayfirst=True).strftime('%d/%m/%Y')
+        except:
+            return fecha
+    
+    
+    # Aplicar la función a la columna 'fecha'
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(unificar_fecha)
+    
+    
+    df = df.apply(lambda x: x.astype(str).str.replace("_","-"))
+    df = df.apply(lambda x: x.astype(str).str.replace("-"," "))
+    df = df.apply(lambda x: x.astype(str).str.replace("$",""))
+    df = df.apply(lambda x: x.astype(str).str.replace(",",""))
+    df = df.apply(lambda x: x.str.lower())
+    
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+      
+    df.drop_duplicates(inplace=True)
     return df
